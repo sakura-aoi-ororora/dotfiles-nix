d3ab8b1f12a5c2db2280bd599fcdeb9d86363cf2
sakura-aoi-ororora <souyakoji@gmail.com>
2024-10-13 11:59:07 +0000

    add Lazygit and vimdoc-ja

--- neovim/.nix
+++ neovim/.nix
@@ -1,4 +1,4 @@
-_inputs: {
+{ pkgs, ... }: {
   programs = {
     nixvim = {
       enable = true;
@@ -15,7 +15,21 @@ _inputs: {
 
       globals = { mapleader = " "; };
 
-      imports = [ ./lsp.nix ./filer.nix ];
+      plugins = { which-key.enable = true; };
+
+      extraPlugins = [
+        (pkgs.vimUtils.buildVimPlugin {
+          name = "vimdoc-ja";
+          src = pkgs.fetchFromGitHub {
+            owner = "vim-jp";
+            repo = "vimdoc-ja";
+            rev = "1e7738adca1a6f1751e21b999d0529451889bb78"; # 10/12/2024
+            sha256 = "sha256-zSq7vVeJod++Js1fhT0qKndP0YTtH8wvNgocHXX34D8=";
+          };
+        })
+      ];
+
+      imports = [ ./lsp.nix ./filer.nix ./git.nix ];
     };
   };
 }
