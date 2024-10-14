a0b238b8585b6482f171452edc411fcf7fc1f4b2
sakura-aoi-ororora <souyakoji@gmail.com>
2024-10-14 11:39:23 +0000

    add lualine & hlchunk

--- neovim/ui.nix
+++ neovim/ui.nix
@@ -1,4 +1,4 @@
-_inputs: {
+{ pkgs, ... }: {
   keymaps = [
     {
       action = "<Cmd>BufferPrevious<CR>";
@@ -12,6 +12,38 @@ _inputs: {
       mode = "n";
       options.desc = "Buffer: Choose Next (>)";
     }
+    {
+      action = "<Cmd>BufferCloseAllButCurrent<CR>";
+      key = "<Leader>bc";
+      mode = "n";
+      options.desc = "Buffer: [c]lose Buffers";
+    }
   ];
   plugins.barbar.enable = true;
+
+  plugins.lualine = {
+    enable = true;
+    settings.options.theme = "OceanicNext";
+  };
+
+  extraPlugins = [{
+    plugin = pkgs.vimUtils.buildVimPlugin {
+      name = "hlchunk.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "shellRaining";
+        repo = "hlchunk.nvim";
+        rev = "ba6e2347177fec8ec6af4ae26d11a468c9e0ca72";
+        sha256 = "sha256-Rx5kpjfpiH9i/IvOXx+wEWSO4gdfmXdhULDxbcBJQAY=";
+      };
+    };
+    config = ''
+      lua << EOF
+      require("hlchunk").setup {
+          chunk = {
+              enable = true
+          }
+      }
+      EOF
+    '';
+  }];
 }
